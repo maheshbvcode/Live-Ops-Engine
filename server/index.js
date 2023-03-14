6d98a428db5561b1dfd9a5827b988e4070fc695c
Mahesh BV <76767461+maheshbvcode@users.noreply.github.com>
2023-03-14 21:24:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 const app = express();
 const userRoutes = require("./routes/user");
 const offerRoutes = require("./routes/offer")
-mongoose.connect("mongodb+srv://mahesh:maheshbv@cluster0.v1dvyhe.mongodb.net/?retryWrites=true&w=majority",).then(()=>{
+mongoose.connect("",).then(()=>{
         console.log("mongodb connected successfully ")
     }).catch(()=>{
         console.log("Failed to connect to database")
